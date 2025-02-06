f801f799352a48609b6de24af7bb2a7bef239981
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-05 19:36:04 -0500

    Update function.tf

--- function.tf
+++ function.tf
@@ -7,29 +7,29 @@ resource "azurerm_resource_group" "rg_benito" {
 
 # Création du plan de service pour la Function App
 resource "azurerm_service_plan" "plan_app_benito" {
-  name                     = "function-app-service-plan"
-  location                 = azurerm_resource_group.rg_benito.location
-  resource_group_name      = azurerm_resource_group.rg_benito.name
-  sku_name                 = "S1"
-  os_type                  = "Linux"
+  name                = "function-app-service-plan"
+  location            = azurerm_resource_group.rg_benito.location
+  resource_group_name = azurerm_resource_group.rg_benito.name
+  sku_name            = "S1"
+  os_type             = "Linux"
 }
 
 # Création du compte de stockage
 resource "azurerm_storage_account" "storage_benito" {
   name                     = "functionappstoragebenito"  # Nom en minuscules
-  resource_group_name       = azurerm_resource_group.rg_benito.name
+  resource_group_name      = azurerm_resource_group.rg_benito.name
   location                 = azurerm_resource_group.rg_benito.location
-  account_tier              = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 # Création de la Function App
 resource "azurerm_linux_function_app" "app_benito" {
-  name                      = "yourfirstnamemcitfunction"
-  location                  = azurerm_resource_group.rg_benito.location
-  resource_group_name       = azurerm_resource_group.rg_benito.name
-  service_plan_id           = azurerm_service_plan.plan_app_benito.id
-  storage_account_name      = azurerm_storage_account.storage_benito.name
+  name                       = "yourfirstnamemcitfunction"
+  location                   = azurerm_resource_group.rg_benito.location
+  resource_group_name        = azurerm_resource_group.rg_benito.name
+  service_plan_id            = azurerm_service_plan.plan_app_benito.id
+  storage_account_name       = azurerm_storage_account.storage_benito.name
   storage_account_access_key = azurerm_storage_account.storage_benito.primary_access_key
 
   site_config {
@@ -45,23 +45,25 @@ resource "azurerm_web_application_firewall_policy" "waf_benito" {
 
   # Bloc de règles gérées
   managed_rules {
-    rule_set_type    = "OWASP"
-    rule_set_version = "3.2"
+    managed_rule_set {
+      type    = "OWASP"       # Type de règles gérées
+      version = "3.2"         # Version des règles
+    }
   }
 
   # Bloc de règles personnalisées
   custom_rules {
-    name     = "allow_all"
-    priority = 1
-    action   = "Allow"
+    name      = "allow_all"
+    priority  = 1
+    rule_type = "MatchRule"  # Type de règle
+    action    = "Allow"      # Action de la règle
 
     match_conditions {
       match_variables {
-        variable_name = "RequestUri"
+        variable_name = "RequestUri"  # Variable à vérifier
       }
-      operator = "Equals"
-      values   = ["/"]  # Condition de correspondance pour la racine
+      operator     = "Equals"         # Opérateur de correspondance
+      match_values = ["/"]            # Valeurs à vérifier
     }
-    rule_type = "Match"  # Ajout de l'argument "rule_type"
   }
 }
