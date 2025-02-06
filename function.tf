0676d83c7f61907edb587c96655f5d9155c46248
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-05 19:11:51 -0500

    Update function.tf

--- function.tf
+++ function.tf
@@ -1,4 +1,6 @@
-
+provider "azurerm" {
+  features {}
+}
 
 # Création du groupe de ressources
 resource "azurerm_resource_group" "rg_benito" {
@@ -13,22 +15,20 @@ resource "azurerm_service_plan" "plan_app_benito" {
   resource_group_name      = azurerm_resource_group.rg_benito.name
   kind                     = "Linux"
   reserved                 = true
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  sku_name                 = "S1"
+  os_type                  = "Linux"
 }
 
 # Création du compte de stockage
 resource "azurerm_storage_account" "storage_benito" {
-  name                     = "functionappstoragebenito47"  
+  name                     = "functionappstoragebenito"  # Nom en minuscules
   resource_group_name       = azurerm_resource_group.rg_benito.name
   location                 = azurerm_resource_group.rg_benito.location
   account_tier              = "Standard"
   account_replication_type = "LRS"
 }
 
-# Création de la Function App avec les arguments manquants
+# Création de la Function App
 resource "azurerm_function_app" "app_benito" {
   name                      = "yourfirstnamemcitfunction"
   location                  = azurerm_resource_group.rg_benito.location
@@ -44,7 +44,12 @@ resource "azurerm_web_application_firewall_policy" "example" {
   name                = "benitomcitfunction"
   resource_group_name = azurerm_resource_group.rg_benito.name
   location            = azurerm_resource_group.rg_benito.location
-  rule_type           = "Match"
+
+  managed_rule_set {
+    rule_set_type    = "OWASP"
+    rule_set_version = "3.2"
+  }
+
   custom_rules {
     name     = "allow_all"
     priority = 1
@@ -56,10 +61,5 @@ resource "azurerm_web_application_firewall_policy" "example" {
       operator = "Equals"
       values   = ["/"]
     }
-  }
-
-  managed_rules {
-    rule_set_type = "OWASP"
-    rule_set_version = "3.2"
   }
 }
